0a9dd1d8e2f3de170937ab3373c4e30ed08b39c3
Fernanda Inacio <fp_inacio@hotmail.com>
2021-06-03 20:21:17 -0300

    Criação calculadora IMC

---
+++ CalculadoraIMC/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    public class Pessoa 
+    {
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public double Imc { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa(double peso, double altura)
+        {
+            Altura = altura;
+            Peso = peso;
+
+        }
+        public Pessoa(double imc)
+        {
+            Imc = imc;
+
+        }
+
+        public Pessoa()
+        {
+
+        }
+        public double Resultado()
+        {
+
+            var Imc = Peso / (Altura * Altura);
+            return Imc;
+        }
+    }
+
+}
---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** CALCULADORA IMC **********");
+
+            Console.WriteLine("Digite o seu nome: ");
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o seu peso: ");
+            pessoa.Peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a sua altura: ");
+            pessoa.Altura = double.Parse(Console.ReadLine());
+            double resultadoImc = pessoa.Resultado();
+
+            Console.WriteLine("********** RESULTADO ********** ");
+            Console.WriteLine("Nome: " + pessoa.Nome);
+            Console.WriteLine("Peso: " + pessoa.Peso);
+            Console.WriteLine("Altura: " + pessoa.Altura);
+            Console.WriteLine("IMC: " + Math.Round(resultadoImc, 2));
+            Console.ReadLine();
+        }
+    }
+}
